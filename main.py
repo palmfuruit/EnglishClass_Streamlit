a0c573d648514a3aa8584e0e69061a5adbfaf1dd
Palm <palmfuruit46@gmail.com>
2024-08-10 15:21:16 +0900

    FastAPIと連携して分類結果を取得

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np 
 from PIL import Image, ImageDraw
 import streamlit as st
+import requests
 import ocr_main
 
 ocr_model = ocr_main.initialize_ocr()
@@ -14,15 +15,16 @@ if 'processed_files' not in st.session_state:
 
 # アップロードされた画像の表示とOCR処理
 image_files = st.file_uploader('画像ファイルを選択してください。', type=['jpg', 'jpeg', 'png'], accept_multiple_files=True)
-if image_files:
+
+# 未処理のファイルを取り出し
+unprocessed_files = [img for img in image_files if img.name not in st.session_state.processed_files]
+
+if unprocessed_files:
     overWrite = st.empty()
-    for idx, img in enumerate(image_files):
-        # ファイル名を使って、同じファイルが2度処理されないようにする
-        if img.name in st.session_state.processed_files:
-            continue
+    for idx, img in enumerate(unprocessed_files):
 
         with overWrite.container():
-            st.write(f'{idx + 1} / {len(image_files)} 枚目の画像を処理しています・・・。')
+            st.write(f'{idx + 1} / {len(unprocessed_files)} 枚目の画像を処理しています・・・。')
 
         original_image = Image.open(img)
         # OCR処理を行い、セッションステートのsentencesに結果を追加
@@ -31,7 +33,14 @@ if image_files:
         # 処理済みのファイル名を保存
         st.session_state.processed_files.add(img.name)
 
-    overWrite.empty()
+    else:
+        overWrite.empty()       # 「N枚目の画像を処理しています・・・。」　消去
+
+        # 取得した文章を文型予測した配列を取得
+        api_url = "http://localhost:8000/predict"
+        data = {"text": st.session_state.sentences}
+        response = requests.post(api_url, json=data)
+        response.raise_for_status()
 
 
 sentence_patterns = [
