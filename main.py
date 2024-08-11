947895b81f4d63362fb7f155804d3746761e0d87
Palm <palmfuruit46@gmail.com>
2024-08-11 21:31:09 +0900

    選択した分の文法情報を出力。

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np 
+import pandas as pd
 from PIL import Image, ImageDraw
 import streamlit as st
 import requests
@@ -49,7 +50,7 @@ if unprocessed_files:
         response = requests.post(api_url, json=data)
         response.raise_for_status()
         response_data = response.json()
-        st.write(response_data)
+        # st.write(response_data)
 
         st.session_state.sentences1 = [st.session_state.sentences[i] for i in range(len(response_data)) if response_data[i]["pattern"] == 1]
         st.session_state.sentences2 = [st.session_state.sentences[i] for i in range(len(response_data)) if response_data[i]["pattern"] == 2]
@@ -89,4 +90,126 @@ elif current_pattern == sentence_patterns[3]:
 elif current_pattern == sentence_patterns[4]:
     selected_sentences = st.session_state.sentences5
 
-st.write(selected_sentences)
+# st.write(selected_sentences)
+# ラジオボタンでテキストを選択
+selected_text = ""
+if selected_sentences:
+    selected_text = st.radio("Select text", selected_sentences)
+
+
+
+import spacy
+
+# SpaCyの英語モデルをロード
+nlp = spacy.load('en_core_web_sm')
+
+
+def get_subtree_span(token):
+    # トークンのサブツリー（そのトークンを含むすべての子孫ノード）の範囲を取得
+    subtree_tokens = list(token.subtree)
+    return subtree_tokens[0].idx, subtree_tokens[-1].idx + len(subtree_tokens[-1].text)
+
+def underline_clauses(sentence):
+    # 文を解析
+    doc = nlp(sentence)
+    
+    spans = []
+
+    for token in doc:
+        if token.dep_ in {'nsubj', 'csubj', 'nsubjpass', 'csubjpass'}:  # 主語
+            span = get_subtree_span(token)
+            clause_type = 'main' if token.head.dep_ == 'ROOT' else 'subordinate'
+            spans.append((span, 'subject', clause_type))
+        elif token.pos_ == 'VERB':  # 動詞
+            span = (token.idx, token.idx + len(token.text))
+            clause_type = 'main' if token.dep_ == 'ROOT' else 'subordinate'
+            spans.append((span, 'verb', clause_type))
+        elif token.dep_ in {'dobj', 'iobj'}:  # 目的語
+            span = get_subtree_span(token)
+            clause_type = 'main' if token.head.dep_ == 'ROOT' else 'subordinate'
+            if token.dep_ == 'dobj':
+                spans.append((span, 'direct_object', clause_type))  # 直接目的語
+            else:
+                spans.append((span, 'indirect_object', clause_type))  # 間接目的語
+        elif token.dep_ in {'attr', 'acomp', 'oprd'}:  # 補語
+            span = get_subtree_span(token)
+            clause_type = 'main' if token.head.dep_ == 'ROOT' else 'subordinate'
+            spans.append((span, 'complement', clause_type))
+        elif token.dep_ == 'aux':  # 助動詞
+            span = (token.idx, token.idx + len(token.text))
+            clause_type = 'main' if token.head.dep_ == 'ROOT' else 'subordinate'
+            spans.append((span, 'auxiliary', clause_type))
+
+    # HTMLとCSSを使って下線と色を追加
+    annotated_sentence = sentence
+    for span, span_type, clause_type in sorted(spans, key=lambda x: x[0][0], reverse=True):
+        if span_type == 'subject':
+            color = 'blue'
+        elif span_type == 'verb':
+            color = 'red'
+        elif span_type == 'direct_object':
+            color = 'green'
+        elif span_type == 'indirect_object':
+            color = 'yellowgreen'
+        elif span_type == 'complement':
+            color = 'orange'
+        elif span_type == 'auxiliary':  # 助動詞
+            color = 'pink'
+
+        if clause_type == 'main':
+            style = f"border-bottom: 2px solid {color};"
+        else:  # 'subordinate'
+            style = f"border-bottom: 2px double {color};"
+
+        annotated_sentence = (
+            annotated_sentence[:span[0]] + 
+            f"<span style='{style}'>{annotated_sentence[span[0]:span[1]]}</span>" + 
+            annotated_sentence[span[1]:]
+        )
+    
+    return annotated_sentence, doc
+
+
+
+def display_legend():
+    legend_html = """
+    <div style='border: 2px solid black; padding: 10px; margin-bottom: 20px;'>
+        <p><span style='color: blue;'>■</span> 主語 (Subject)</p>
+        <p><span style='color: red;'>■</span> 動詞 (Verb)</p>
+        <p><span style='color: green;'>■</span> 直接目的語 (Direct Object)</p>
+        <p><span style='color: yellowgreen;'>■</span> 間接目的語 (Indirect Object)</p>
+        <p><span style='color: orange;'>■</span> 補語 (Complement)</p>
+        <p><span style='color: pink;'>■</span> 助動詞 (Auxiliary)</p>
+        <p><span style='border-bottom: 2px solid black; display: inline-block; width: 80px;'>      </span> 主節 (Main Clause)</p>
+        <p><span style='border-bottom: 2px double black; display: inline-block; width: 80px;'>      </span> 従属節 (Subordinate Clause)</p>
+    </div>
+    """
+    st.markdown(legend_html, unsafe_allow_html=True)
+
+
+def display_token_info(doc):
+    token_data = {
+        "Text": [token.text for token in doc],
+        "Lemma": [token.lemma_ for token in doc],
+        "POS": [token.pos_ for token in doc],
+        "Tag": [token.tag_ for token in doc],
+        "Dependency": [token.dep_ for token in doc],
+        "Head": [token.head.text for token in doc],
+        "Children": [[child.text for child in token.children] for token in doc],
+        "Start": [token.idx for token in doc],
+        "End": [token.idx + len(token.text) for token in doc]
+    }
+    
+    token_df = pd.DataFrame(token_data)
+    st.dataframe(token_df)
+
+
+st.divider() # 水平線
+if selected_text:
+    underlined_text, doc = underline_clauses(selected_text)
+    st.markdown(underlined_text, unsafe_allow_html=True)
+    display_token_info(doc)
+
+
+# 凡例を表示
+display_legend()
